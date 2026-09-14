b9492b6546fee04567d99578b089171699c9d718
Aaron Yoon <awesomeyooner@gmail.com>
2026-09-13 20:16:06 -0700

    removed build folder

--- eeprom_test/firmware/build/cube/CMakeFiles/firmware.dir/startup_stm32f446xx.s.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/firmware.dir/startup_stm32f446xx.s.obj: \
- /home/aaron/Documents/GitHub/PSD-17DB/eeprom_test/firmware/cube/startup_stm32f446xx.s
--- eeprom_test/firmware/build/lib/WireLib/CMakeFiles/WireLib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for WireLib.
-# This may be replaced when dependencies are built.
