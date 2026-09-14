eb993af73fbb167ba979964767a98c1d6f29f48c
Aaron Yoon <awesomeyooner@gmail.com>
2026-09-14 00:19:09 -0700

    Update core.cpp

--- eeprom_test/firmware/src/core.cpp
+++ eeprom_test/firmware/src/core.cpp
@@ -35,51 +35,105 @@ void deselect()
 } // end of "select()"
 
 
-void EEPROM_WriteEnable()
+void EEPROM_write_enable()
 {
     uint8_t command = EEPROM_WREN;
 
     select();
 
-    HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
+    HAL_SPI_Transmit(&hspi2, &command, sizeof(command), HAL_MAX_DELAY);
     
     deselect();
      
-} // end of "EEPROM_WriteEnable"
+} // end of "EEPROM_write_enable()"
 
 
-void EEPROM_ReadBuffer(uint16_t address, uint8_t *pBuffer, uint16_t size) {
-    uint8_t header[3];
-    header[0] = EEPROM_READ;
-    header[1] = (uint8_t)(address >> 8);   // Address MSB
-    header[2] = (uint8_t)(address & 0xFF);  // Address LSB
+// Datasheet Page 23 - 6.5 READ
+// Instruction is first, then 16 bit address (MSB First)
+void EEPROM_read_bytes(uint16_t address, uint8_t* read_buffer, uint16_t num_bytes)
+{
+    uint8_t command[3];
 
+    command[0] = EEPROM_READ;
+
+    // MSB (First 8 Bits)
+    command[1] = (uint8_t)(address >> 8);
+
+    // LSB (Last 8 Bits)
+    command[2] = (uint8_t)(address & 0xFF);
+
+    // Select SPI
     select();
-    // Transmit 3-byte command frame header
-    HAL_SPI_Transmit(&hspi2, header, 3, HAL_MAX_DELAY);
-    // Receive the requested data bytes immediately after
-    HAL_SPI_Receive(&hspi2, pBuffer, size, HAL_MAX_DELAY);
+
+    // Transmit the READ command
+    HAL_SPI_Transmit(&hspi2, command, sizeof(command), HAL_MAX_DELAY);
+
+    // Receive however many bytes
+    HAL_SPI_Receive(&hspi2, read_buffer, num_bytes, HAL_MAX_DELAY);
+
+    // Deselect SPI
     deselect();
-}
+
+} // end of "EEPROM_read_bytes"
 
 
-void EEPROM_WritePage(uint16_t address, uint8_t *pData, uint16_t size) {
-    uint8_t header[3];
-    header[0] = EEPROM_WRITE;
-    header[1] = (uint8_t)(address >> 8);
-    header[2] = (uint8_t)(address & 0xFF);
+// Datasheet Page 20 - 6.3.4 SRWD Bit
+// b0 is the Write In Progress Bit
+// It is 1 if Write in Progress
+// and 0 when it is ready to be written to
+void EEPROM_wait_ready()
+{
+    uint8_t command = EEPROM_RDSR;
+    uint8_t status_reg = 0;
 
-    EEPROM_WriteEnable(); // Must be set before every write command
+    do 
+    {
+        // Read the Status Register
+        select();
+        HAL_SPI_Transmit(&hspi2, &command, sizeof(command), 100);
+        HAL_SPI_Receive(&hspi2, &status_reg, sizeof(status_reg), 100);
+        deselect();
+        
+        // Keep looping while WIP bit (b0) is 1 (when write is in progress))
+    } 
+    while ((status_reg & 0x01) == 0x01); 
+
+} // end of "EEPROM_wait_ready()"
+
+
+// Datasheet Page 24 - 6.6 WRITE
+// Instruction is first, then 16 bit address (MSB First)
+void EEPROM_write_bytes(uint16_t address, uint8_t* write_buffer, uint16_t num_bytes)
+{
+    uint8_t command[3];
+
+    command[0] = EEPROM_WRITE;
+
+    // MSB (First 8 Bits)
+    command[1] = (uint8_t)(address >> 8);
+
+    // LSB (Last 8 Bits)
+    command[2] = (uint8_t)(address & 0xFF);
+
+    EEPROM_write_enable();
     
     select();
-    HAL_SPI_Transmit(&hspi2, header, 3, HAL_MAX_DELAY);
-    HAL_SPI_Transmit(&hspi2, pData, size, HAL_MAX_DELAY);
+
+    HAL_SPI_Transmit(&hspi2, command, sizeof(command), HAL_MAX_DELAY);
+
+    HAL_SPI_Transmit(&hspi2, write_buffer, num_bytes, HAL_MAX_DELAY);
+
     deselect();
     
-    HAL_Delay(5); // Wait 5ms for physical EEPROM programming cycle
-}
+    // HAL_Delay(5);
+    EEPROM_wait_ready();
+
+} // end of "EEPROM_write_bytes()"
 
 
+// Datasheet Page 20 - 6.3.4 SRWD Bit
+// b6 to b4 are guarenteed 0, so if we AND the Status Register
+// with 0xFF, then if the result isn't == 0xFF then the EEPROM responded
 bool EEPROM_is_connected()
 {
     uint8_t command = EEPROM_RDSR;
@@ -87,36 +141,25 @@ bool EEPROM_is_connected()
 
     select();
 
-    if (HAL_SPI_Transmit(&hspi2, &command, 1, 100) == HAL_OK) {
-        HAL_SPI_Receive(&hspi2, &status_reg, 1, 100);
-    }
+    if (HAL_SPI_Transmit(&hspi2, &command, sizeof(EEPROM_RDSR), 100) == HAL_OK)
+        HAL_SPI_Receive(&hspi2, &status_reg, sizeof(status_reg), 100);
 
     deselect();
 
+    // If nothing changed
+    // Then the EEPROM didn't respond
     if(status_reg == 0xFF)
         return false;
     
+    // If bits b6 -  b4 aren't 0'd
+    // Then the EEPROM didn't respond
+    // Since Status Register guarentees them to be zero'd
     if((status_reg & 0x70) != 0x00)
         return false;
 
     return true;
-}
 
-
-bool EEPROM_wait_ready()
-{
-    uint8_t command = EEPROM_RDSR;
-    uint8_t status_reg;
-
-    do {
-        select();
-        HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
-        HAL_SPI_Receive(&hspi2, &status_reg, 1, HAL_MAX_DELAY);
-        deselect();
-        
-        // Loop while Bit 0 (WIP) is 1
-    } while ((status_reg & 0x01) == 0x01); 
-}
+} // end of "EEPROM_is_connected()"
 
 
 GPIODevice led = GPIODevice(GPIOC, GPIO_PIN_1);
@@ -132,15 +175,15 @@ void init()
     if(!EEPROM_is_connected())
         led.set_high();
 
-    double data = 3.14159;
+    double data = 150.15;
 
-    EEPROM_WritePage(0, (uint8_t*)&data, sizeof(double));
+    EEPROM_write_bytes(0, (uint8_t*)&data, sizeof(double));
 
     // EEPROM_wait_ready();
 
     double read = 0;
 
-    EEPROM_ReadBuffer(0, (uint8_t*)&read, sizeof(double));
+    EEPROM_read_bytes(0, (uint8_t*)&read, sizeof(double));
 
     HAL_Delay(500);
 
